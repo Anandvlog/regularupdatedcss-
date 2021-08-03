cca0be24efad52ad0b2c6b2ecf5e6feb52c00775
Anand Chauhan <71122607+Anandvlog@users.noreply.github.com>
2021-08-03 11:14:00 +0530

    Add files via upload

---
+++ typed_text/main.js
@@ -0,0 +1,13 @@
+var typed = new Typed('.anim', {
+    // Waits 1000ms after typing "First"
+    strings: [
+        "Developer",
+        "Designer",
+        "Photoshop"
+    ],
+      typeSpeed:60,
+      backSpeed:60,
+      loop:true
+
+
+    });
